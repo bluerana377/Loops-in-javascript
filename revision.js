5400146a38fcdd97d536a3e8f8a341fc00b15741
Bluerana377 <hasansohel492@gmail.com>
2023-08-06 22:39:32 +0400

    added some js file

---
+++ revision.js
@@ -0,0 +1,18 @@
+// string type variable
+var bottle = 'water';
+var bottleColor = 'yellow';
+// number type variable 
+var sunglassPrice = 120;
+var penColor = blue;
+// boolean type variable 
+var isExpensive = true;
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+
+var item3 = tableItems[3];
+tableItems[4] = 'notebook';
+var penIndex = tableItems.indexOf('pen');
+//  push and pop
+
+// conditional
+// using if and else if then else 
